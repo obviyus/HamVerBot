0652e4c2d233dc897c61ee03c6419078f3afcc24
Ayaan Zaidi <biblioklept@icloud.com>
2022-03-26 23:53:18 +0530

    [result] handle Result and Options properly

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,5 @@
-use std::{result, str::FromStr};
-
 use chrono::DateTime;
+use log::error;
 use rusqlite::{params, Connection, Error};
 
 use crate::models::Timetable;
@@ -23,7 +22,7 @@ struct EventResult {
 pub fn init_db() -> Result<(), Error> {
     let conn = Connection::open("hamverbot.db")?;
 
-    conn.execute(
+    match conn.execute(
         "CREATE TABLE IF NOT EXISTS events (
             id INTEGER PRIMARY KEY,
             meeting_name TEXT NOT NULL,
@@ -31,109 +30,130 @@ pub fn init_db() -> Result<(), Error> {
             start_time INTEGER NOT NULL
          )",
         [],
-    )?;
+    ) {
+        Ok(_) => {}
+        Err(e) => return Err(e),
+    };
 
-    conn.execute(
+    match conn.execute(
         "CREATE TABLE IF NOT EXISTS results (
             id INTEGER PRIMARY KEY,
             path TEXT NOT NULL UNIQUE,
             end_time INTEGER NOT NULL
          )",
         [],
-    )?;
+    ) {
+        Ok(_) => {}
+        Err(e) => return Err(e),
+    };
 
     Ok(())
 }
 
-pub fn insert_events(meeting_name: &str, timetable: Vec<&Timetable>) -> Result<(), ()> {
+pub fn insert_events(meeting_name: &str, timetable: Vec<&Timetable>) {
     let conn = Connection::open("hamverbot.db").unwrap();
 
     timetable
         .iter()
         .map(|event| {
-            let start_time = chrono::DateTime::parse_from_str(
+            let start_time = match chrono::DateTime::parse_from_str(
                 &format!("{} {}", &event.start_time, &event.gmt_offset),
                 "%Y-%m-%dT%H:%M:%S %:z",
-            )
-                .unwrap();
+            ) {
+                Ok(start_time) => start_time,
+                Err(e) => {
+                    error!("Failed to parse start_time: {}", e);
+                    return;
+                }
+            };
 
-            conn.execute(
+            match conn.execute(
                 "INSERT INTO events (meeting_name, description, start_time) VALUES (?1, ?2, ?3)",
                 params![meeting_name, event.description, start_time.timestamp()],
-            )
+            ) {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("Failed to insert event: {}", e);
+                }
+            };
         })
         .last();
-
-    Ok(())
 }
 
-pub fn next_event() -> Result<Option<(String, String, chrono::DateTime<chrono::Utc>)>, Error> {
+pub fn next_event() -> Option<(String, String, chrono::DateTime<chrono::Utc>)> {
     let conn = Connection::open("hamverbot.db").unwrap();
 
     let timestamp_now = chrono::Utc::now().timestamp();
+
     let mut stmt =
-        conn.prepare("SELECT *, MIN(start_time - ?1) FROM events WHERE start_time > ?2")?;
-    let mut event_iter = stmt.query_map(
-        params![timestamp_now, timestamp_now], |row| {
+        match conn.prepare("SELECT *, MIN(start_time - ?1) FROM events WHERE start_time > ?2") {
+            Ok(stmt) => stmt,
+            Err(e) => {
+                error!("Error preparing statement: {}", e);
+                return None;
+            }
+        };
+
+    let mut event_iter = stmt
+        .query_map(params![timestamp_now, timestamp_now], |row| {
             Ok(Event {
                 id: row.get(0)?,
                 meeting_name: row.get(1)?,
                 description: row.get(2)?,
                 start_time: row.get(3)?,
             })
-        })?;
-
-    if let Some(event) = event_iter.next() {
-        return match event {
-            Ok(event) => {
-                Ok(Some((
-                    format!(
-                        "🏎️ \x02{}\x02: {} begins in 5 minutes.",
-                        event.meeting_name, event.description
-                    ),
-                    format!("{}: {}", event.meeting_name, event.description),
-                    DateTime::from_utc(
-                        chrono::NaiveDateTime::from_timestamp(event.start_time, 0),
-                        chrono::Utc,
-                    ),
-                )))
-            }
-            Err(e) => {
-                println!("ERROR: {:?}", e);
-                Ok(None)
-            }
-        }
-    }
-
-    Ok(None)
-}
-
-pub fn insert_result(path: &str, end_time: chrono::DateTime<chrono::Utc>) -> Result<(), ()> {
-    let conn = Connection::open("hamverbot.db").unwrap();
-    println!("SAVING: {:?}", path);
-
-    conn.execute(
-        "INSERT INTO results (path, end_time) VALUES (?1, ?2)",
-        params![path, end_time.timestamp()],
-    )
+        })
         .unwrap();
 
-    println!("SAVING: {:?}", path);
-
-    Ok(())
+    return if let Some(event) = event_iter.next() {
+        match event {
+            Ok(event) => Some((
+                format!(
+                    "🏎️ \x02{}\x02: {} begins in 5 minutes.",
+                    event.meeting_name, event.description
+                ),
+                format!("{}: {}", event.meeting_name, event.description),
+                DateTime::from_utc(
+                    chrono::NaiveDateTime::from_timestamp(event.start_time, 0),
+                    chrono::Utc,
+                ),
+            )),
+            Err(e) => {
+                error!("Failed to get next event: {}", e);
+                return None;
+            }
+        }
+    } else {
+        None
+    };
 }
 
-pub fn is_result_sent(path: &str) -> Result<bool, Error> {
-    println!("SEARCHING: {:?}", path);
+pub fn insert_result(
+    path: &str,
+    end_time: chrono::DateTime<chrono::Utc>,
+) -> Result<(), reqwest::Error> {
     let conn = Connection::open("hamverbot.db").unwrap();
 
-    let count: Option<i64> = conn
-        .query_row(
-            "SELECT COUNT(id) FROM results WHERE path = ?1",
-            &[path],
-            |r| r.get(0),
-        )
-        .expect("select failed");
+    return match conn.execute(
+        "INSERT INTO results (path, end_time) VALUES (?1, ?2)",
+        params![path, end_time.timestamp()],
+    ) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            error!("Failed saving result to DB: {}", e);
+            Ok(())
+        }
+    };
+}
 
-    Ok(count.unwrap_or(0) > 0)
+pub fn is_result_sent(path: &str) -> bool {
+    let conn = Connection::open("hamverbot.db").unwrap();
+
+    conn.query_row(
+        "SELECT COUNT(id) FROM results WHERE path = ?1",
+        &[path],
+        |r| r.get(0),
+    )
+    .unwrap_or(0)
+        > 0
 }
--- src/event_results.rs
+++ src/event_results.rs
@@ -3,6 +3,7 @@ use crate::{
     models::{DriverStanding, SPFeed, SessionInfo},
 };
 use chrono::DateTime;
+use log::{error, info};
 use reqwest::Client;
 
 const F1_SESSION_ENDPOINT: &str = "https://livetiming.formula1.com/static";
@@ -17,41 +18,50 @@ pub async fn read_result() -> Result<Option<String>, reqwest::Error> {
         .text_with_charset("utf-8-sig")
         .await?;
 
+    // Strip BOM from UTF-8-SIG
     let session: SessionInfo = serde_json::from_str(body.trim_start_matches('\u{feff}')).unwrap();
-    println!("FOUND: {:?}", session.path);
 
-    match database::is_result_sent(&session.path) {
-        Ok(false) => {
-            if session.archive_status.status == "Complete" {
-                println!("{} {}", &session.end_date, &session.gmt_offset);
+    if !database::is_result_sent(&session.path) && session.archive_status.status == "Complete" {
+        // TODO: Do we really need to store the end_time?
+        let end_time = match chrono::DateTime::parse_from_str(
+            &format!("{} +{}", &session.end_date, &session.gmt_offset),
+            "%Y-%m-%dT%H:%M:%S %:z:00",
+        ) {
+            Ok(end_time) => end_time,
+            Err(e) => {
+                error!("Failed to parse end_time: {}", e);
+                return Ok(None);
+            }
+        };
 
-                let end_time = chrono::DateTime::parse_from_str(
-                    &format!("{} +{}", &session.end_date, &session.gmt_offset),
-                    "%Y-%m-%dT%H:%M:%S %:z:00",
-                )
-                .unwrap();
+        // TODO: Update the topic of the IRC channel with data for the next event
 
-                database::insert_result(
-                    &session.path,
-                    DateTime::from_utc(end_time.naive_utc(), chrono::Utc),
-                )
-                .unwrap();
+        match database::insert_result(
+            &session.path,
+            DateTime::from_utc(end_time.naive_utc(), chrono::Utc),
+        ) {
+            Ok(_) => {
+                info!("Saving result for {}", &session.path);
+            }
+            Err(e) => {
+                error!("Failed to insert result: {}", e);
+                return Ok(None);
+            }
+        };
 
-                let result_string = driver_standings(client, session.path).await.unwrap();
-                return Ok(Some(result_string));
-            };
-
-            return Ok(None);
-        }
-        Ok(true) => {
-            println!("SKIPPING: {:?}", session.path);
-            return Ok(None);
-        }
-        Err(e) => {
-            println!("ERROR_READ_RESULT: {:?}", e);
-            return Ok(None);
-        }
-    };
+        return match driver_standings(client, session.path.clone()).await {
+            Ok(formatting_string) => {
+                info!("Delivering result for {}", session.path);
+                Ok(Some(formatting_string))
+            }
+            Err(e) => {
+                error!("Failed to get driver standings: {}", e);
+                Ok(None)
+            }
+        };
+    } else {
+        Ok(None)
+    }
 }
 
 async fn driver_standings(client: Client, path: String) -> Result<String, reqwest::Error> {
@@ -62,9 +72,8 @@ async fn driver_standings(client: Client, path: String) -> Result<String, reqwes
         .text_with_charset("utf-8-sig")
         .await?;
 
-    println!("FOUND: {:?}", body);
+    // Strip BOM from UTF-8-SIG
     let session: SPFeed = serde_json::from_str(body.trim_start_matches('\u{feff}')).unwrap();
-
     let mut standings = session
         .free
         .data
@@ -86,21 +95,24 @@ async fn driver_standings(client: Client, path: String) -> Result<String, reqwes
             }
         })
         .collect::<Vec<DriverStanding>>();
-
     standings.sort_by(|a, b| a.position.cmp(&b.position));
+
+    info!(
+        "Pulled results for {}: {}",
+        session.free.data.r, session.free.data.s
+    );
 
     let mut output = format!(
         "🏎️ \x02{}: {} Results\x02:",
         session.free.data.r, session.free.data.s
     );
 
+    // Consider only the first 10 drivers to avoid spamming
     for standing in standings.iter().take(10) {
-        let rank = format!(
+        output.push_str(&format!(
             " {}. {} - \x0303[{}]\x03",
             standing.position, standing.driver_name, standing.time
-        );
-
-        output.push_str(&rank);
+        ));
     }
 
     Ok(output)
